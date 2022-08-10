38b88f13245230c4651d8bf776c6d03d1db8f454
Pab-lo <salaberry.pablo@gmail.com>
2022-08-10 19:26:30 +0200

    making the api call and http methos

--- nextjs-course-code-f09d94f9a61700bc544a90edb53bdaaf3c4c7f8a/src/pages/NewMeetup.js
+++ nextjs-course-code-f09d94f9a61700bc544a90edb53bdaaf3c4c7f8a/src/pages/NewMeetup.js
@@ -1,10 +1,28 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
+import {useHistory} from 'react-router-dom';
 
 function NewMeetup() {
+    const history = useHistory();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-refresher-682c0-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'content-type': 'application/json'
+                }
+            }
+            ).then(() => {
+                history.replace('/');
+            });
+    }
+
     return ( 
     <section>
         <h1>Add a new meetup</h1>
-        <NewMeetupForm /> 
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/> 
     </section>
     );
 }
