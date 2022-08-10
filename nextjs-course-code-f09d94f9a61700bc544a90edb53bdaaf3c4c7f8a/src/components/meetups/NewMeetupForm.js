138563aac5a4124df18e3ec43b7dcc303c0ac93b
Pab-lo <salaberry.pablo@gmail.com>
2022-08-10 17:22:32 +0200

    Form handler added

--- nextjs-course-code-f09d94f9a61700bc544a90edb53bdaaf3c4c7f8a/src/components/meetups/NewMeetupForm.js
+++ nextjs-course-code-f09d94f9a61700bc544a90edb53bdaaf3c4c7f8a/src/components/meetups/NewMeetupForm.js
@@ -1,5 +1,53 @@
-function NewMeetupForm() {
+import Card from '../UI/Card';
+import classes from './NewMeetupForm.module.css';
+import { useRef } from 'react';
 
+function NewMeetupForm() {
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const adressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
+    function submitHandler(event) {
+        event.preventDefault();
+        const enteredTitle = titleInputRef.current.value;
+        const enteredImage = imageInputRef.current.value;
+        const enteredAddress = adressInputRef.current.value;
+        const enteredDescription = descriptionInputRef.current.value;
+
+        const meetupData ={
+            title: enteredTitle,
+            Image: enteredImage,
+            address: enteredAddress,
+            description: enteredDescription,
+        } 
+
+        console.log(meetupData)
+    }
+
+    return <Card>
+        <form className={classes.form} onSubmit={submitHandler}>
+            <div className={classes.control}>
+                <label htmlFor='title'>Meetup Title</label>
+                <input type="text" required id='title' ref={titleInputRef} />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='image'>Meetup image</label>
+                <input type="url" required id='image' ref={imageInputRef} />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='Adress'>Adress</label>
+                <input type="text" required id='Adress' ref={adressInputRef} />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='Description'>Description</label>
+                <textarea type="text" required id='Description' ref={descriptionInputRef} />
+            </div>
+            <div className={classes.actions}>
+                <button>Add Meetup</button>
+            </div>
+        </form>
+    </Card>
 }
 
 export default NewMeetupForm;
--- nextjs-course-code-f09d94f9a61700bc544a90edb53bdaaf3c4c7f8a/src/pages/NewMeetup.js
+++ nextjs-course-code-f09d94f9a61700bc544a90edb53bdaaf3c4c7f8a/src/pages/NewMeetup.js
@@ -1,5 +1,13 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 function NewMeetup() {
-    return <div>NewMeetup page</div>
-}  
+    return ( 
+    <section>
+        <h1>Add a new meetup</h1>
+        <NewMeetupForm /> 
+    </section>
+    );
+}
+
 
 export default NewMeetup;
