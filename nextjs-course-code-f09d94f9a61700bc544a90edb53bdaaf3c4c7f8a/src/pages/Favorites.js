eb5cae1eed2db80447dee7cdd42da9ad4d193945
Pab-lo <salaberry.pablo@gmail.com>
2022-08-11 20:27:18 +0200

    fixing bugs

--- nextjs-course-code-f09d94f9a61700bc544a90edb53bdaaf3c4c7f8a/src/pages/Favorites.js
+++ nextjs-course-code-f09d94f9a61700bc544a90edb53bdaaf3c4c7f8a/src/pages/Favorites.js
@@ -1,12 +1,25 @@
 import { useContext } from 'react';
 
-import FavoritesConectext from '../store/favorites-context';
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/Meetuplist';
 
-function Favorites() {
-    const favoritesCtx = useContext(FavoritesContext)
-    return <section>
-        <h1>My Favorites</h1>
+function FavoritesPage() {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
     </section>
-}  
+  );
+}
 
-export default Favorites;
+export default FavoritesPage;
