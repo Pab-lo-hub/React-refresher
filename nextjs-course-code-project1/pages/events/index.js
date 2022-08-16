4ce04b8a519cdee6b8d1d9a3edfa15d2a719bac9
Pab-lo <salaberry.pablo@gmail.com>
2022-08-16 12:58:43 +0200

    fixing details

--- nextjs-course-code-project1/pages/events/index.js
+++ nextjs-course-code-project1/pages/events/index.js
@@ -1,6 +1,5 @@
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
-
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
